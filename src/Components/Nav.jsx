0a58a1a57235a0002ad8bbe059fb87b67746ebdc
Princessayomide53 <94536831+Princessayomide53@users.noreply.github.com>
2023-01-29 16:49:56 -0500

    fixed the navbar dropdown

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -9,15 +9,15 @@ import {  Squash as Hamburger } from 'hamburger-react';
 
 function Nav() {
 
-  const [nav, setNav] = useState(false);
+  // const [nav, setNav] = useState(false);
 
   const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
   const [menu_class, setMenuClass] = useState("menu hidden1")
   const [isMenuClicked, setIsMenuClicked] = useState(false);
 
-  const handleNav = () => {
-    setNav(!nav);
-  };
+  // const handleNav = () => {
+  //   setNav(!nav);
+  // };
 
   const updateMenu = () => {
     if (!isMenuClicked) {
@@ -68,57 +68,70 @@ function Nav() {
         </div>
       </div>
 
-      <div className='lg:hidden block'>
-      <div className="flex justify-between bg-black pt-2 pb-2 fixed w-full z-10">
-        <div className=" pl-7 pt-2">
-          <h2 className="text1 text-4xl font-bold ">PA.</h2>
+      <div className="lg:hidden block">
+        <div className="flex justify-between bg-black pt-2 pb-2 fixed w-full z-10">
+          <div className=" pl-7 pt-2">
+            <h2 className="text1 text-4xl font-bold ">PA.</h2>
+          </div>
+          <div className='mr-7'>
+            <Hamburger
+              direction="squash"
+              size={30}
+              color="white"
+              className="burger-menu"
+              onToggle={updateMenu}
+            />
+          </div>
         </div>
-        <Hamburger
-          direction="squash"
-          size={30}
-          color="white"
-          className="burger-menu"
-          onToggle={updateMenu}
-        />
-      </div>
-      <div className={menu_class}>
-        <div className="text-white">
-          <ul className="space-y-8 p-5 -mt-28 absolute top-72 left-20">
-            <motion.li
-              initial={animateFrom}
-              animate={animateTo}
-              transition={{ delay: 0.1 }}
-              className="border-b-2 pb-1 border-gray-500 w-56 hover:text-[#ff4b1f]"
-            >
-              {/* <a smooth href="#hash-link-aboutme"> */}
-              About Me
-              {/* </a> */}
-            </motion.li>
-            <motion.li
-              initial={animateFrom}
-              animate={animateTo}
-              transition={{ delay: 0.15 }}
-              className="border-b-2 pb-1 border-gray-500 w-56 hover:text-[#ff4b1f]"
-            >
-              {/* <a smooth href="#hash-link-aboutme"> */}
-              Technologies
-              {/* </a> */}
-            </motion.li>
-            <motion.li
-              initial={animateFrom}
-              animate={animateTo}
-              transition={{ delay: 0.2 }}
-              className="border-b-2 pb-1 border-gray-500 w-56 hover:text-[#ff4b1f]"
-            >
-              {/* <a smooth href="#hash-link-projects"> */}
-              Projects
-              {/* </a> */}
-            </motion.li>
-          </ul>
+        <div className={menu_class}>
+          <div className="text-white bg-black w-full h-screen fixed">
+            <ul className="space-y-8 md:space-y-14 p-5 -mt-28 absolute top-72 left-16 md:left-64">
+              <motion.li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.2 }}
+                className="border-b-2 pb-1 md:pb-2 border-gray-500 w-56 hover:text-[#ff4b1f]"
+              >
+                <a smooth href="#hash-link-aboutme">
+                  Resumes
+                </a>
+              </motion.li>
+              <motion.li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.3 }}
+                className="border-b-2 pb-1 md:pb-2 border-gray-500 w-56 hover:text-[#ff4b1f]"
+              >
+                <a smooth href="#hash-link-technologies">
+                  Technologies
+                </a>
+              </motion.li>
+              <motion.li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.4 }}
+                className="border-b-2 pb-1 md:pb-2 border-gray-500 w-56 hover:text-[#ff4b1f]"
+              >
+                <a smooth href="#hash-link-projects">
+                  Projects
+                </a>
+              </motion.li>
+
+              <motion.li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.45 }}
+                className="border-b-2 pb-1 border-gray-500 w-56 hover:text-[#ff4b1f]"
+              >
+                <a smooth href="#hash-link-calls">
+                  Contact
+                </a>
+              </motion.li>
+            </ul>
+          </div>
         </div>
       </div>
-      </div>
-      
+
       {/* <div className="wrap lg:hidden md:block  block">
         <div className="nav1 flex justify-between mt-20">
           <div className=" pl-7 pt-2">
